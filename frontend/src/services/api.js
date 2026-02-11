725fcbeea5b1f42a37e6ac63d09aa88d26bc7743
Chris-gsoto <chris@gscodecr.com>
2026-02-10 18:31:01 -0600

    Implementar sesion por usuario

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -24,7 +24,7 @@ const hideWakeup = () => {
 
 // Agregar token + detectar servidor lento
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
+    const token = sessionStorage.getItem('token');
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
